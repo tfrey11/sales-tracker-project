d6b542f5b408421274b088b31b7b060c23eeebc8
Tyler Frey <tylerfrey2011@gmail.com>
2023-08-19 16:34:48 -0500

    started work on database model

--- model.py
+++ model.py
@@ -1,1 +1,61 @@
 """Models for sales tracker app"""
+
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Salesperson(db.Model):
+    """A salesperson"""
+
+    __tablename__ = "salespeople"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    fname = db.Column(db.String)
+    lname = db.Column(db.String)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    sales = db.relationship("Sale", back_populates="salespeople")
+
+
+    def __repr__(self):
+        return f"<Salesperson id={self.id} name={self.fname} {self.lname}>"
+    
+class Sale(db.Model):
+    """A sale"""
+
+    __tablename__ = "sales"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    sales_person_id = db.Column(db.Integer, db.ForeignKey("salespeople.id"))
+    customer_id = db.Column(db.Integer, db.ForeignKey("customers.id"))
+    car_make = db.Column(db.String)
+    car_model = db.Column(db.String)
+    car_year = db.Column(db.Integer)
+    sell_price = db.Column(db.Integer)
+    sale_date = db.Column(db.Datetime)
+
+    customers = db.relationship("Customer", back_populates="sales")
+    salesperson = db.relationship("Salesperson", back_populates="sales")
+
+    def __repr__(self):
+        return f"<Sale id={self.id} salesperson={self.sales_person_id} date={self.sale_date}>"
+    
+class Customer(db.Model):
+    """A customer"""
+
+    __tablename__ = "customers"
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    customer_fname = db.Column(db.String)
+    customer_lname = db.Column(db.String)
+    customer_email = db.Column(db.String)
+    customer_phone_number = db.Column(db.String)
+
+    purchases = db.relationship("Sales", back_populates="customers")
+
+    def __repr__(self):
+        return f"<Customer customer_id={self.id} customer_name={self.customer_fname} {self.customer_lname}>"
+    
+    
